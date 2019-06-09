85504872482cccddad7e1ba3d6b630abccc14ed5
johnwalley <john@walley.org.uk>
2019-06-09 13:24:20 +0100

    Generate better library output

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,56 @@
-const webpack = require("webpack");
-const path = require("path");
-const autoprefixer = require("autoprefixer");
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: "./index.js",
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  externals: {
+    react: 'react,',
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+    },
+  },
   output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "index.js",
-    library: ["react-rowing-blades"],
-    libraryTarget: "umd",
-    publicPath: "/lib/",
-    globalObject: "this"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-rowing-blades.js',
+    library: ['react-rowing-blades'],
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader?modules",
-        include: /flexboxgrid/
+        loader: 'style-loader!css-loader?modules',
+        include: /flexboxgrid/,
       },
       {
         test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
         test: /\.svg$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       options: {
         context: __dirname,
-        postcss: [autoprefixer]
-      }
-    })
-  ]
+        postcss: [autoprefixer],
+      },
+    }),
+  ],
 };
