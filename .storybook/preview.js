d27afaf8e6ab0d586caf315789e0cd73d0f398df
johnwalley <john@walley.org.uk>
2021-01-02 17:13:41 +0000

    docs: update intro and add getting started section

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  options: {
+    storySort: {
+      order: ["Example", ["Introduction", "Getting started"], "Components"],
+    },
+  },
+};
